c00f49c6d346f46cc8d2f93ca2f1914891443d7d
wajdan <wejdan95.almamari@gmail.com>
2026-06-23 08:14:49 +0400

    Add Aircraft model

---
+++ FlightManagementSystem/Models/Aircraft.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlightManagementSystem.Models
+{
+    public class Aircraft
+    {
+        public int AircraftId { get; set; }
+        public string Model { get; set; }
+        public int TotalSeats { get; set; }
+        public bool IsOperational { get; set; }
+    }
+}
