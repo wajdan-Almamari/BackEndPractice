ba567682b3b10c30be8fc64b7c3a40d4b903b7b6
wajdan <wejdan95.almamari@gmail.com>
2026-06-23 09:08:22 +0400

    Add Flight model

---
+++ FlightManagementSystem/Models/Flight.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlightManagementSystem.Models
+{
+    public class Flight
+    {
+        public int FlightId { get; set; } // System Generated
+        public string FlightCode { get; set; } // System Generated
+        public int AircraftId { get; set; } // From List
+        public int PilotId { get; set; } // From List
+        public string Origin { get; set; } // User Input
+        public string Destination { get; set; } // User Input
+        public string DepartureDate { get; set; } // User Input
+        public string DepartureTime { get; set; } // User Input
+        public decimal TicketPrice { get; set; } // User Input
+        public int AvailableSeats { get; set; } // Calculated
+        public string Status { get; set; } // Default Value
+    }
+}
