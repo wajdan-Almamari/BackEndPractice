4cbaaa74d543a53283c7cd002c8db0a85bb174eb
wajdan <wejdan95.almamari@gmail.com>
2026-06-23 09:31:12 +0400

    Add FlightContext storage

--- FlightManagementSystem/Program.cs
+++ FlightManagementSystem/Program.cs
@@ -1,7 +1,17 @@
-﻿namespace FlightManagementSystem
+﻿using FlightManagementSystem.Models;
+
+namespace FlightManagementSystem
 {
-    internal class Program
+    public class Program
     {
+        public static FlightContext context = new FlightContext
+        {
+            Passengers = new List<Passenger>(),
+            Pilots = new List<Pilot>(),
+            Aircrafts = new List<Aircraft>(),
+            Flights = new List<Flight>(),
+            Bookings = new List<Booking>()
+        };
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
