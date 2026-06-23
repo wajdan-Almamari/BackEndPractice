1348d7f1e8939d3441aebb096d65146ef783fa1a
wajdan <wejdan95.almamari@gmail.com>
2026-06-23 09:12:23 +0400

    Add Booking model

---
+++ FlightManagementSystem/Models/Booking.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlightManagementSystem.Models
+{
+    public class Booking
+    {
+        public int BookingId { get; set; } // System Generated
+        public int PassengerId { get; set; } // From List
+        public int FlightId { get; set; } // From List
+        public string SeatNumber { get; set; } // System Generated
+        public string BookingDate { get; set; } // System Generated
+        public decimal TotalPrice { get; set; } // Calculated
+        public string Status { get; set; } // Default Value
+    }
+}
