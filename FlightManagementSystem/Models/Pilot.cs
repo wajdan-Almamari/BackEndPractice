8474a99d339e2da698fcd6f147952967826b9faa
wajdan <wejdan95.almamari@gmail.com>
2026-06-23 08:31:24 +0400

    Add Pilot model

--- FlightManagementSystem/Models/Pilot.cs
+++ FlightManagementSystem/Models/Pilot.cs
@@ -6,11 +6,11 @@ namespace FlightManagementSystem.Models
 {
     public class Pilot
     {
-        public int PilotId { get; set; }
-        public string PilotName { get; set; }
-        public string PilotPhone { get; set; }
-        public string LicenseNumber { get; set; }
-        public int FlightHours { get; set; }
-        public bool IsAvailable { get; set; }
+        public int PilotId { get; set; }//System Generated
+        public string PilotName { get; set; }//User Input
+        public string PilotPhone { get; set; }//User Input
+        public string LicenseNumber { get; set; }//User Input
+        public int FlightHours { get; set; }//Default Value
+        public bool IsAvailable { get; set; }//Default Value
     }
 }
