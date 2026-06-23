ee90db7f07183f98787e54486bee81f89550316e
wajdan <wejdan95.almamari@gmail.com>
2026-06-23 08:08:23 +0400

    Add Passenger model

---
+++ FlightManagementSystem/Models/Passenger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlightManagementSystem.Models
+{
+    internal class Passenger
+    {
+        public int PassengerId { get; set; }
+        public string PassengerName { get; set; }
+        public string PassengerEmail { get; set; }
+        public string PassengerPhone { get; set; }
+        public string PassportNumber { get; set; }
+        public string Nationality { get; set; }
+    }
+}
