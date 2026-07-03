da6e390f36373f6ed42d205644d10d46c45510ee
wajdan-Almamari <wejdan95.almamari@gmail.com>
2026-07-03 23:03:13 +0400

    Add ECommerce context

---
+++ E_Commerce_SystemERD_Models/ECommerceContext.cs
@@ -0,0 +1,24 @@
+﻿using E_Commerce_SystemERD_Models.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_Commerce_SystemERD_Models
+{
+    internal class ECommerceContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=localhost;Database=ECommerceDB;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+
+    }
+}
