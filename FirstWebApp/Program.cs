016f9f5ed5c839fedd09b66cd73307d5c5776337
wajdan <wejdan95.almamari@gmail.com>
2026-07-14 07:54:57 +0400

    initial migration

--- FirstWebApp/Program.cs
+++ FirstWebApp/Program.cs
@@ -11,14 +11,14 @@ namespace FirstWebApp
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+           // builder.Services.AddOpenApi();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+               // app.MapOpenApi();
             }
 
             app.UseHttpsRedirection();
--- FirstWebApp/ProjectContext.cs
+++ FirstWebApp/ProjectContext.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FirstWebApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace FirstWebApp
 {
     public class ProjectContext : DbContext
     {
+        public DbSet<Product> Products { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=DESKTOP-U403BM4;Database=ProjectDataStore;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
     }
 }
